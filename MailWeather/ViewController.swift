e0212eaa4fa21287c4cd957edc21ff00f8bee93c
Emelyanov Konstantin <1924gg@gmail.com>
2020-11-13 15:34:56 +0300

    Update ViewController.swift

--- MailWeather/ViewController.swift
+++ MailWeather/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let searchBar = UISearchBar()
+    let cityView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(searchBar)
         
+        view.addSubview(cityView)
     }
 
+    func setupConstraints() {
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        cityView.translatesAutoresizingMaskIntoConstraints = false
+        
+        [
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            searchBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+        
+            cityView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 20),
+            cityView.leadingAnchor.constraint(equalTo: searchBar.leadingAnchor),
+            cityView.trailingAnchor.constraint(equalTo: searchBar.trailingAnchor),
+            cityView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+    }
+    
+    override func viewDidLayoutSubviews() {
+        setupConstraints()
+    }
 }
 
