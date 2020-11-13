f31bc3a0cb95483c82f53ef5cee45c86b07a697d
Emelyanov Konstantin <1924gg@gmail.com>
2020-11-13 15:22:17 +0300

    Initial Commit

---
+++ MailWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MailWeather
+//
+//  Created by Константин Емельянов on 13.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
+
